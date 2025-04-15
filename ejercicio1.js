c64b7aacf0497d6f3582c7d3910cda32db89c655
LeonardoCostilla <leosape650@gmail.com>
2025-04-15 16:54:10 -0300

    Commit avances

---
+++ ejercicio1.js
@@ -0,0 +1,3 @@
+//variable de integrantes
+let integrantes = "Leoanardo, Fernando y Franco";
+console.log("Somos " + integrantes + " y estamos aprendiendo javascrpit.");
---
+++ ejercicio4.js
@@ -0,0 +1,7 @@
+/* Calcula el area de un cuadrado */
+let lado = 8;
+let area = 2 * lado;
+let perimetro = lado + lado + lado + lado;
+
+console.log("Area del cuadrado: " + area);
+console.log("Perimetro del cuadrado: " + perimetro);
