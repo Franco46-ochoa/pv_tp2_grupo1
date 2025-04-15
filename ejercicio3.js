8f49fe52f2eb611244a9cf8534e9747f5074c5d9
Franco46-ochoa <46323573@fi.unju.edu.ar>
2025-04-14 21:32:12 -0300

    Commit ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,8 @@
+const r= 7.5;
+const Area= (radio) => {
+    const area = Math.PI * radio * radio;
+    console.log ('El área del círculo con radio ' + radio + ' es: ' + area);
+  };
+  
+Area(r);
+  
