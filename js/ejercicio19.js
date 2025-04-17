8be829113a65a077883754527cc5fbdaf22e15bb
LeonardoCostilla <leosape650@gmail.com>
2025-04-17 08:32:42 -0300

    Commit ejercicio 19

--- js/ejercicio19.js
+++ js/ejercicio19.js
@@ -3,14 +3,14 @@
 // Obtenemos los elementos
 const inputNombre = document.querySelector("#nombre");
 const inputApellido = document.querySelector("#apellido");
-const inputLibreta = document.querySelector("#libreta");
+const inputLua = document.querySelector("#libreta");
 const boton = document.querySelector("#mostrarDatos");
 
 // Evento en el botón
 boton.addEventListener("click", () => {
   const nombre = inputNombre.value.trim();
   const apellido = inputApellido.value.trim();
-  const libreta = inputLibreta.value.trim();
+  const libreta = inputLua.value.trim();
 
   alert(`Los datos ingresados son:\n Nombre: ${nombre} Apellido: ${apellido} Libreta Universitaria: ${libreta}`);
 });
